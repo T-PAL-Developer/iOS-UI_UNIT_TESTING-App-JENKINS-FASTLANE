86673c7cc1920f216add94c4f30092dfa97e9ff0
Tomasz Plauszkiewicz <tpal.developer@gmail.com>
2021-01-07 19:02:28 +0100

    Add UI testing

---
+++ wallcalcaroniUITests/wallcalcaroniUITests.swift
@@ -0,0 +1,31 @@
+//
+//  wallcalcaroniUITests.swift
+//  wallcalcaroniUITests
+//
+//  Created by Tomasz Paluszkiewicz on 07/01/2021.
+//  Copyright © 2021 Tomasz Paluszkiewicz. All rights reserved.
+//
+
+import XCTest
+
+class wallcalcaroniUITests: XCTestCase {
+
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+    }
+
+    override func tearDownWithError() throws {
+        app = nil
+    }
+
+    func testButtons_WhenTapped_ShouldShowResult() {
+        app.buttons["1"].tap()
+        app.buttons["2"].tap()
+        XCTAssertTrue(app.staticTexts["12"].exists)
+    }
+  
+}
